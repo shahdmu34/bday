198e730fb6ed507d9f25fcc3426afb78e6b6a7c6
Shahd Mustafa <99287558+shahdmu34@users.noreply.github.com>
2025-11-13 17:00:21 -0500

    STYLE COMMIT

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import cake80 from './assets/80.png';
 import cake60 from './assets/60.png';
 import cake40 from './assets/40.png';
 import cake20 from './assets/20.png';
-
+import star from './assets/star-nobg.png';
 
 function App() {
   // images to rotate through (add/remove as needed)
@@ -27,15 +27,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="outer-contianer">
-          <div className="main-contianer">
-            <p>Happy Birthday</p>
-            <img
-              src={images[index]}
-              alt={`cake-${index + 1}`}
-              className="cake-image"
-            />
-          </div>
+        <div className="main-contianer">
+          <img src={star} alt="star" className="border-star" />
+          <img src={star} alt="star" className="border-star2" />
+          <img src={star} alt="star" className="border-star3" />
+          <img src={star} alt="star" className="border-star4" />
+
+
+          <p>HAPPY BIRTHDAY</p>
+          <img
+            src={images[index]}
+            alt={`cake-${index + 1}`}
+            className="cake-image"
+          />
         </div>
       </header>
     </div>
